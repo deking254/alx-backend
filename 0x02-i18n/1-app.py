6c154a61becc2247bf296d582c1a08c5740766e5
deking254 <dekingsky@gmail.com>
2024-01-11 11:54:22 +0000

    removed the app.run

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """using the flask_babel"""
-from flask_babel import Babel as babel
+from flask_babel import Babel
 import flask
 app = flask.Flask(__name__)
-
+babel = Babel(app)
 class Config():
     """the configuration class for the app"""
     LANGUAGES = ["en", "fr"]
-"""app.run('0.0.0.0', 5000)"""
