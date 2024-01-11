6049ddc3c08d48ecb3d6e34a2545d7b2986ed5f0
deking254 <dekingsky@gmail.com>
2024-01-11 10:58:44 +0000

    added an init func to the config class

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -6,6 +6,8 @@ app = flask.Flask(__name__)
 babel = Babel().init_app(app)
 
 class Config():
-    LANGUAGES = ["en", "fr"]
+    """the configuration class for the app"""
+    def __init__(self):
+        self.LANGUAGES = ["en", "fr"]
 
 app.run('0.0.0.0', 5000)
