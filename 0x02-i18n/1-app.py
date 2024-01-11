07925400be9521dc9eb452a653cd432f364c27bb
deking254 <dekingsky@gmail.com>
2024-01-11 11:10:28 +0000

    created the babel instance

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -3,11 +3,10 @@
 from flask_babel import Babel
 import flask
 app = flask.Flask(__name__)
-babel = Babel().init_app(app)
+babel = Babel(app)
 
 class Config():
     """the configuration class for the app"""
-    def __init__(self):
-        self.LANGUAGES = ["en", "fr"]
+    LANGUAGES = ["en", "fr"]
 
 app.run('0.0.0.0', 5000)
