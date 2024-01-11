c4ff455a5ce6348d63dad8d67b8af8650d3a5880
deking254 <dekingsky@gmail.com>
2024-01-11 11:47:01 +0000

    imported babel as babel

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 """using the flask_babel"""
-from flask_babel import Babel
+from flask_babel import Babel as babel
 import flask
 app = flask.Flask(__name__)
-babel = Babel(app)
 
 class Config():
     """the configuration class for the app"""
     LANGUAGES = ["en", "fr"]
-print(locals())
-app.run('0.0.0.0', 5000)
+"""app.run('0.0.0.0', 5000)"""
