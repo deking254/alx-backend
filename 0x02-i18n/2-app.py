822b77aaa41c177c6204e195a371620a096b0e08
deking254 <dekingsky@gmail.com>
2024-01-12 07:09:51 +0000

    created the get locale function

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+"""using the flask_babel"""
+from flask_babel import Babel
+from flask import Flask, request
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config():
+    """the configuration class for the app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def get_locale():
+    """get the locale from request"""
+    a = request.accept_languages
+    t = a.best_match(app.config['LANGUAGES'])
+    return 'The best fit locale is {}'.format(t)
